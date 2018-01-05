af2a1a92fdc2ee51a1ff047c0d04a797c562383c
Matt Traudt <sirmatt@ksu.edu>
2018-01-05 15:15:13 -0500

    Add ability to specify multiple inputs in plot-cdf.py

--- src/plot-cdf.py
+++ src/plot-cdf.py
@@ -29,34 +29,58 @@ def getcdf(data):
         lasty = frac[i]
     return x, y
 
-def main(args):
+def get_all_values_from_fd(fd):
     values = []
-    for line in sys.stdin:
+    for line in fd:
         line = line.strip() 
         try: values.append(float(line))
         except: print('ignoring', line)
-    if args.max_points >= 0 and len(values) > args.max_points:
-        values = random.sample(values, args.max_points)
-    with PdfPages(args.output) as pdf:
-        plt.figure()
+    return values
+
+def main(args, pdf):
+    plt.figure()
+    if not args.input:
+        values = get_all_values_from_fd(sys.stdin)
+        if args.max_points >= 0 and len(values) > args.max_points:
+            values = random.sample(values, args.max_points)
         plt.plot(*getcdf(values))
-        plt.ylim(ymin=0, ymax=1)
         plt.xlim(xmin=min(values), xmax=max(values))
-        if args.label: plt.xlabel(args.label)
-        if args.title: plt.title(args.title)
-        plt.ylabel('CDF')
-        pdf.savefig()
+    else:
+        all_min, all_max = None, None
+        for fname, label in args.input:
+            with open(fname, 'rt') as fd:
+                values = get_all_values_from_fd(fd)
+            if args.max_points >= 0 and len(values) > args.max_points:
+                values = random.sample(values, args.max_points)
+            this_min, this_max = min(values), max(values)
+            if all_min == None or this_min < all_min: all_min = this_min
+            if all_max == None or this_max > all_max: all_max = this_max
+            plt.plot(*getcdf(values), label=label)
+        plt.xlim(xmin=all_min, xmax=all_max)
+        plt.legend(loc='lower right')
+    plt.ylim(ymin=0, ymax=1)
+    if args.label: plt.xlabel(args.label)
+    if args.title: plt.title(args.title)
+    plt.ylabel('CDF')
+    pdf.savefig()
 
 if __name__=='__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,
-        description='Take a list of values on stdin, one value per line, and '
-        'plot a CDF of them')
+        description='Takes one or more lists of values, one per line, and '
+        'plots a CDF of them. Values may either be given on stdin or read '
+        'from one or more files.')
+    parser.add_argument('-i', '--input', nargs=2, metavar=('FNAME', 'LABEL'),
+        action='append', help='Specify a file to read values from and what '
+        'to label its line in the PDF. If given 0 times, data will be '
+        'read from stdin. Can be given more than once. The only time data '
+        'will be read from stdin is if it is given 0 times.')
     parser.add_argument('-o', '--output', default='temp.pdf')
     parser.add_argument('-l', '--label', type=str,
-        help='What to label the data in the PDF')
+        help='What to label the X axis in the PDF')
     parser.add_argument('-t', '--title', type=str,
         help='What to title the plot in the PDF')
     parser.add_argument('--max-points', default=10000, type=int,
-        help='Negative means infinite')
+        help='Maximum number of points on a line. Negative means infinite')
     args = parser.parse_args()
-    exit(main(args))
+    with PdfPages(args.output) as pdf:
+        exit(main(args, pdf))
