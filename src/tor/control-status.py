e37bdf62847519568fbde240fd7c0c8760b1e461
Matt Traudt <sirmatt@ksu.edu>
2018-02-09 17:33:30 -0500

    PEP8 and such

--- src/tor/control-status.py
+++ src/tor/control-status.py
@@ -3,7 +3,6 @@ from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 import time
 from stem.control import Controller
 from curses import wrapper
-from datetime import datetime
 
 def myprint(s, *words):
     s.addstr(' '.join([str(w) for w in words])+'\n')
@@ -12,21 +11,25 @@ def print_stats(scr, cont):
     all_circs = []
     all_streams = cont.get_streams()
     for circ in cont.get_circuits():
-        path = [ nick for fp, nick in circ.path ]
-        streams = [ s for s in all_streams if s.circ_id == circ.id ]
-        all_circs.append({'id': int(circ.id),
+        path = [nick for fp, nick in circ.path]
+        streams = [s for s in all_streams if s.circ_id == circ.id]
+        all_circs.append({
+            'id': int(circ.id),
             'path': path,
             'used': True if len(streams) else False,
-            'targets': [ s.target for s in streams ],
-            })
+            'targets': [s.target for s in streams],
+        })
     all_circs = sorted(all_circs, key=lambda c: c['id'])
     for circ in all_circs:
         s = '*' if circ['used'] else ' '
         s += ' {} {}'.format(circ['id'], ' '.join(circ['path']))
-        if circ['used']: s += ' (' + ','.join(circ['targets']) + ')'
+        if circ['used']:
+            s += ' (' + ','.join(circ['targets']) + ')'
         myprint(scr, s)
-    myprint(scr, len(all_streams),'streams; {}/{} circuits in use'.format(
-            len([c for c in all_circs if c['used'] ]), len(all_circs)))
+    myprint(
+        scr, len(all_streams), 'streams; {}/{} circuits in use'.format(
+            len([c for c in all_circs if c['used']]), len(all_circs)))
+
 
 def main(stdscr, args):
     stdscr.clear()
@@ -42,8 +45,9 @@ def main(stdscr, args):
 def pre_main():
     parser = ArgumentParser(
             formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-p', '--ctrl-port', metavar='PORT', type=int,
-            help='Port on which to control the tor client', default=9051)
+    parser.add_argument(
+        '-p', '--ctrl-port', metavar='PORT', type=int,
+        help='Port on which to control the tor client', default=9051)
     parser.add_argument('-i', '--interval', metavar='SECS', type=float,
             help='How often to update screen', default=1.0)
     args = parser.parse_args()
