8c4df9c6db5a138299b6a2bd700d4bfd1bfbfcee
Matt Traudt <sirmatt@ksu.edu>
2019-05-23 10:19:02 -0400

    Add split_x_by_y

--- src/batch.py
+++ src/batch.py
@@ -9,3 +9,22 @@ def batch(iterable, n = 1):
            current_batch = []
    if current_batch:
        yield current_batch
+
+
+def split_x_by_y(x, y):
+    ''' Divide X as evenly as possible Y ways using only ints, and return those
+    ints. Consider x=5 and y=3. 5 cannot be divided into 3 pieces evenly using
+    ints. This function would yield a generator producing 1, 2, 2.
+
+    x=8, y=5 yields 1, 2, 1, 2, 2.
+    x=6, y=3 yields 2, 2, 2
+    '''
+    frac_accum = 0
+    for iters_left in range(y-1, 0-1, -1):
+        frac_accum += x % y
+        if frac_accum >= y or not iters_left and frac_accum:
+            yield x // y + 1
+        else:
+            yield x // y
+        if frac_accum >= y:
+            frac_accum -= y
